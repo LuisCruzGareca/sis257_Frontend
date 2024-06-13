77a1f1cddd15a11e790b9fd036b79b69ab646c68
Luis Miguel Cruz Gareca <lc109070@gmail.com>
2024-06-13 05:32:57 -0400

    si el usuario no esta logeado redirigir al login

--- celulares-fronted/src/views/CelularesView.vue
+++ celulares-fronted/src/views/CelularesView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { router } from '@/router/router'
+if (!localStorage.getItem('token')) {
+  router.push('/')
+}
 </script>
 
 <template>
--- celulares-fronted/src/views/ColoresView.vue
+++ celulares-fronted/src/views/ColoresView.vue
@@ -1,4 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { router } from '@/router/router'
+if (!localStorage.getItem('token')) {
+  router.push('/')
+}
+</script>
 
 <template>
   <RouterView ENDPOINT_API="colores"></RouterView>
